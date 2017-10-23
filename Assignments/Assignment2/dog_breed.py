e753b8cb3a25120f0d73ff80cbe4c755bd42262f
liechtia <andrea.liechti@unifr.ch>
2017-10-23 14:19:15 +0200

    split data -> train, test, val
    split data -> train, test, val

--- Assignments/Assignment2/dog_breed.py
+++ Assignments/Assignment2/dog_breed.py
@@ -1,15 +1,33 @@
 import torchvision.models as models
 import torch.nn as nn
 import torch.optim as optim
-
+import csv
+import numpy, os
+from PIL import Image
 import sklearn
+from sklearn.model_selection import train_test_split
 
+# downloaded the data to my computer as i could not find it on the server
+train_path = '../../dogbreeds/train/'
+label_path = '../../dogbreeds/labels.csv'
 
-data_dir  = 'train'
+dog_images = []
+dog_image_ids = []
+labels = {}
+
 # load data and split it into train,validation and test
+for file in os.listdir(train_path):
+    img = Image.open(train_path+"/"+file)
+    featurevector = numpy.array(img).flatten()
+    dog_images.append(featurevector)
+    dog_image_ids.append(os.path.splitext(file)[0])
 
-# X_train,X_test,Y_train,Y_test = train_test_split(X,y,train_size=0.8,test_size=0.2,random_state=42)
-#X_train_2,X_val,Y_train_2,Y_val = train_test_split(X_train,Y_train, train_size= 0.875,random_state=42)
+reader = csv.reader(open(label_path))
+for row in reader:
+    labels[row[0]] = row[1]
+
+X_train,X_test,Y_train,Y_test = train_test_split(dog_images,dog_image_ids,train_size=0.8,test_size=0.2,random_state=42)
+X_train_2,X_val,Y_train_2,Y_val = train_test_split(X_train,Y_train, train_size= 0.875, test_size=0.125,random_state=42)
 
 alexnet_pretrained = models.alexnet(pretrained=True)
 alexnet = models.alexnet()
