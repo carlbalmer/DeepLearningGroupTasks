6eadd4a9052ebd2c81313410762a931e4eca5ec8
meoti <alvinoti27@gmail.com>
2017-10-27 18:47:46 +0200

    just updated

--- Assignments/Assignment2/dog_breed.py
+++ Assignments/Assignment2/dog_breed.py
@@ -1,42 +1,17 @@
-import torchvision.models as models
-import torch.nn as nn
-import torch.optim as optim
-from sklearn.model_selection import train_test_split
+import os
+
 import pandas as pd
-from torch import np # Torch wrapper for Numpy
-
-from torch.utils.data.dataset import Dataset
-from torch.utils.data import DataLoader
-from torchvision import transforms
-
-from torch.autograd import Variable
-
-from sklearn.preprocessing import LabelEncoder
-
-import numpy as np
+import torch.optim as optim
 import torchvision
-
-
-# downloaded the data to my computer as i could not find it on the server
-
-dog_images = []
-dog_image_ids = []
-labels = {}
-
-'''# load data and split it into train,validation and test
-#for file in os.listdir(train_path):
- #   img = Image.open(train_path+"/"+file)
-  #  featurevector = numpy.array(img).flatten()
-   # dog_images.append(featurevector)
-   # dog_image_ids.append(os.path.splitext(file)[0])
-
-reader = csv.reader(open(label_path))
-for row in reader:
-    labels[row[0]] = row[1]
-
-X_train,X_test,Y_train,Y_test = train_test_split(dog_images,dog_image_ids,train_size=0.8,test_size=0.2,random_state=42)
-X_train_2,X_val,Y_train_2,Y_val = train_test_split(X_train,Y_train, train_size= 0.875, test_size=0.125,random_state=42)
-'''
+from PIL import Image
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import LabelEncoder
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import transforms
 
 IMG_PATH = 'dogs/train/'
 IMG_EXT = '.jpg'
@@ -44,7 +19,7 @@ TRAIN_DATA = 'dogs/labels.csv'
 
 
 class DogsDataset(Dataset):
-    """Dataset 
+    """Dataset
     Arguments:
         A CSV file path
         Path to image folder
@@ -53,29 +28,27 @@ class DogsDataset(Dataset):
     """
 
     def __init__(self, csv_path, img_path, img_ext, transform=None):
-    
         tmp_df = pd.read_csv(csv_path)
-        #print(tmp_df)
-       # assert tmp_df['breed'].apply(lambda x: os.path.isfile(img_path + x + img_ext)).all(), \
-#"Some images referenced in the CSV file were not found"
-        
+        # print(tmp_df)
+        assert tmp_df['breed'].apply(lambda x: os.path.isfile(img_path + x + img_ext)).all(), \
+            "Some images referenced in the CSV file were not found"
+
         self.mlb = LabelEncoder()
         self.img_path = img_path
         self.img_ext = img_ext
         self.transform = transform
 
         self.X_train = tmp_df['id']
-        self.y_train = self.mlb.fit_transform(tmp_df['breed'])#having problem shaping it in the right size
-    
-        #print(self.y_train[0])
-        
+        self.y_train = self.mlb.fit_transform(tmp_df['breed'])  # having problem shaping it in the right size
+
+        # print(self.y_train[0])
 
     def __getitem__(self, index):
         img = Image.open(self.img_path + self.X_train[index] + self.img_ext)
         img = img.convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
-        
+
         label = (self.y_train[index])
         return img, label
 
@@ -83,29 +56,34 @@ class DogsDataset(Dataset):
         return len(self.X_train.index)
 
 
-
-    
 transformations = transforms.Compose([
-        transforms.RandomSizedCrop(224),
-       
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+    transforms.RandomSizedCrop(224),
 
-dset_train = DogsDataset(TRAIN_DATA,IMG_PATH,IMG_EXT,transformations)
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
-#split dset using kfold here and make multiple loaders
+dset = DogsDataset(TRAIN_DATA, IMG_PATH, IMG_EXT, transformations)
 
 
+def make_stratified_splits(D_in: DogsDataset):
+    X = D_in.X_train
+    y = D_in.y_train
+    test_straf = StratifiedShuffleSplit(n_splits=1, test_size=0.2, train_size=0.8, random_state=4456)
+
+    train_straf = StratifiedShuffleSplit(n_splits=1, test_size=0.125, train_size=0.875, random_state=58778)
+    rest_index, test_index = next(test_straf.split(X, y))
+    train_index, val_index = next(train_straf.split(X[rest_index], y[rest_index]))
+    return train_index, val_index, test_index
 
 
-train_loader = DataLoader(dset_train,
-                          batch_size=50,
-                          shuffle=True,
-                          #num_workers=4 # 1 for CUDA
-                         # pin_memory=True # CUDA only
-                         )
-#make the net
+train_index, val_index, test_index = make_stratified_splits(dset)
+# define dataloaders
+train_loader = DataLoader(dset, batch_size=50, sampler=SubsetRandomSampler(train_index))
+validation_loader = DataLoader(dset, batch_size=50, sampler=SubsetRandomSampler(val_index))
+test_loader = DataLoader(dset, batch_size=50, sampler=SubsetRandomSampler(test_index))
+
+# make the net
 model = torchvision.models.resnet18(pretrained=True)
 for param in model.parameters():
     param.requires_grad = False
@@ -119,7 +97,7 @@ criterion = nn.CrossEntropyLoss()
 
 # Observe that only parameters of final layer are being optimized as
 # opoosed to before.
-optimizer = optim.SGD(model.fc.parameters(), lr= 1e-3, momentum=0.9)
+optimizer = optim.SGD(model.fc.parameters(), lr=1e-3, momentum=0.9)
 
 
 def train(epoch):
@@ -127,18 +105,18 @@ def train(epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
         # data, target = data.cuda(async=True), target.cuda(async=True) # On GPU
         data, target = Variable(data.cuda()), Variable(target.long().cuda())
-        
+
         optimizer.zero_grad()
         output = model(data)
-        #print( output.size(), target.data)
-        loss = criterion(output, target)        
+        # print( output.size(), target.data)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
-            
-            
+                       100. * batch_idx / len(train_loader), loss.data[0]))
+
+
 for epoch in range(1, 25):
     train(epoch)
