fd8f6ec466feb9bc5d856aaf95e548744786cd20
Sammer <sammer.puran1@students.unibe.ch>
2017-11-20 16:54:05 +0100

    added forward pass to SExpLog

--- Project/SExpLogFunction.py
+++ Project/SExpLogFunction.py
@@ -3,6 +3,7 @@ import torch
 from torch.autograd import Variable
 from torch.autograd import Variable
 from torch.autograd.function import Function, once_differentiable
+from skimage.util.shape import  view_as_blocks,view_as_windows
 from torch._thnn import type2backend
 from . import _all
 
@@ -10,14 +11,18 @@ from . import _all
 class SExpLogFunction(Funcion):
 
     @staticmethod
-    def forward(ctx,input,kernel_size,alphas,padding = 0,stride=None):
+    def forward(ctx,input,kernel_size,alphas,dims):
         ctx.save_for_backward(input,alphas)
         ctx.input = input
         ctx.alphas = alphas
         ctx.kernel_size = kernel_size
-        ctx.padding = padding
-        ctx.stride = stride
-        return input
+
+        h = ctx.alphas*ctx.input
+        he = np.exp(h)
+        B = view_as_blocks(he, block_shape=(kernel_size,kernel_size,dims))
+        output_size_x = input.shape[0]/kernel_size
+        output_size_y = input.shape[1]/kernel_size
+        return np.log(B.sum(axis=(5,4,3))).reshape(output_size_x,output_size_y)
 
         """
            In the backward pass we receive a Tensor containing the gradient of the loss
