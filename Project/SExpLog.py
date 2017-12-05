8e92a299a8af53cf8cc4007426f99f61be2a219f
meoti <alvinoti27@gmail.com>
2017-12-05 02:31:51 +0100

    sexplog function and module

--- Project/SExpLog.py
+++ Project/SExpLog.py
@@ -1,16 +1,22 @@
+import torch
 import torch.nn as nn
 import torch.nn.init as init
 import SExpLogFunction
 
 
 class SExpLog(nn.Module):
-    def __init__(self,input_feautures):
+    def __init__(self,input_feautures, kernel_size,step):
         super(SExpLog,self).__init__()
         self.input_feautures = input_feautures
-        self.alphas = nn.Parameter(torch.Tensor(input_feautures))
+        self.kernel_size = kernel_size
+        self.step = step
+        #alphas shd be input feature size-kernel_size/s + 1
+        height = 1+((input_feautures.size()[0]- kernel_size)/step)
+        width = 1 + ((input_feautures.size()[1] - kernel_size) / step)
+        self.alphas = nn.Parameter(torch.FloatTensor(width,height).zero_())
         #initialize alphas with one
-        init.constant(self.alphas,1)
+        init.constant(self.alphas,1.0)
 
     def forward(self,input):
-        return SExpLogFunction.apply(self,self.input_feautures,2,self.alphas)
+        return SExpLogFunction.apply(self.input_feautures,self.alphas)
 
--- Project/SExpLogFunction.py
+++ Project/SExpLogFunction.py
@@ -5,35 +5,42 @@ from torch.autograd import Variable
 from torch.autograd.function import Function, once_differentiable
 from skimage.util.shape import  view_as_blocks,view_as_windows
 from torch._thnn import type2backend
-from . import _all
+import numpy as np
+#from . import _all  #what is this doiing? @sam
 
 
-class SExpLogFunction(Funcion):
+class SExpLogFunction(Function):
 
     @staticmethod
-    def forward(ctx,input,kernel_size,alphas,dims):
+    def forward(ctx, input, alphas):
         ctx.save_for_backward(input,alphas)
         ctx.input = input
         ctx.alphas = alphas
-        ctx.kernel_size = kernel_size
+        dims = input.shape[2]  # dims shd be taken from the 3rd dim of input
 
-        h = ctx.alphas*ctx.input
-        he = np.exp(h)
-        B = view_as_blocks(he, block_shape=(kernel_size,kernel_size,dims))
-        output_size_x = input.shape[0]/kernel_size
-        output_size_y = input.shape[1]/kernel_size
-        return np.log(B.sum(axis=(5,4,3))).reshape(output_size_x,output_size_y)
+        output_size_x = input.shape[0]/ctx.kernel_size
+        output_size_y = input.shape[1]/ctx.kernel_size
+        #unfold takes in dim, size in each window determined by kernel, and step
+        ctx.re = Variable(ctx.alphas * (ctx.input.unfold(0,ctx.kernel_size,ctx.step).unfold(1,ctx.kernel_size,ctx.step)\
+            .contiguous().view(output_size_x,output_size_y,dims,-1)))
+        #we have a 4 dim tensor, of which we operate the 4th dim to get colapse
 
-        """
+        ctx.sexplog = Variable(torch.log1p(torch.sum(torch.exp(ctx.re),3))) # grad w.r.t sexplog()
+        #sanipoint print functions history to validate grads
+
+        #ctx.save_for_backward(input, alphas,ctx.sexplog)
+        return ctx.sexplog
+    """
            In the backward pass we receive a Tensor containing the gradient of the loss
            with respect to the output, and we need to compute the gradient of the loss
            with respect to the input.
        """
     @staticmethod
-    def backward(self,grad_output):
+    def backward(ctx,grad_output):
 
-        grad_input = grad_output.clone()
-        return grad_input
+        grad_input = grad_output * ctx.sexplog.backward()
+        grad_alphas = grad_output * ctx.re.backward()
+        return grad_input,grad_alphas
 
 
 
