babb5f3283c322d717e76bf42eb86e62cb6fc751
Carl Balmer <carl.balmer@gmail.com>
2017-12-15 03:47:23 +0100

    added SpatialSexplog Model

--- Project/model/__init__.py
+++ Project/model/__init__.py
@@ -3,3 +3,4 @@ from .Pooled_CNN_basic import Pooled_CNN_Basic
 from .Sexy_CNN_basic import Sexy_CNN_Basic
 from .Pooled_CNN_big import Pooled_CNN_big
 from .Sexy_CNN_big import Sexy_CNN_big
+from .Spatial_Sexy_CNN_basic import Spatial_Sexy_CNN_Basic
