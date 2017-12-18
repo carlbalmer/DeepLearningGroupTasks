8e6ea88c0de156e2566550c24d4db66bff7e75a6
Carl Balmer <carl.balmer@gmail.com>
2017-12-18 12:58:40 +0100

    added DOGS spatial SExpLog model

--- Project/model/Spatial_Sexy_CNN_big.py
+++ Project/model/Spatial_Sexy_CNN_big.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 
 
-class Sexy_CNN_big(nn.Module):
+class Spatial_Sexy_CNN_big(nn.Module):
     """
     :var conv1   : torch.nn.Conv2d
     :var conv2   : torch.nn.Conv2d
@@ -20,7 +20,7 @@ class Sexy_CNN_big(nn.Module):
         """
         :param num_classes: the number of classes in the dataset
         """
-        super(Sexy_CNN_big, self).__init__()
+        super(Spatial_Sexy_CNN_big, self).__init__()
         # First layer small 24, alex 64
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 24, kernel_size=7, stride=3),
@@ -28,7 +28,8 @@ class Sexy_CNN_big(nn.Module):
         )
 
         self.pool1 = nn.AvgPool2d(3)
-        self.alpha1 = nn.Parameter(torch.ones(1, 1))
+        self.alpha1 = nn.Parameter(torch.ones(1, 1, 28, 28))
+        self.upscale1 = nn.Upsample(scale_factor=3)
 
         # Second layer small 48, alex 192
         self.conv2 = nn.Sequential(
@@ -37,7 +38,8 @@ class Sexy_CNN_big(nn.Module):
         )
         # Polling layer
         self.pool2 = nn.AvgPool2d(3)
-        self.alpha2 = nn.Parameter(torch.ones(1, 1))
+        self.alpha2 = nn.Parameter(torch.ones(1, 1, 8, 8))
+        self.upscale2 = nn.Upsample(scale_factor=3)
 
         # Third layer small 72, alex 384
         self.conv3 = nn.Sequential(
@@ -58,7 +60,8 @@ class Sexy_CNN_big(nn.Module):
         )
 
         self.pool3 = nn.AvgPool2d(2)
-        self.alpha3 = nn.Parameter(torch.ones(1, 1))
+        self.alpha3 = nn.Parameter(torch.ones(1, 1, 4, 4))
+        self.upscale3 = nn.Upsample(scale_factor=3)
 
         # Classification layer small 768, alex 4096
         self.fc = nn.Sequential(
@@ -74,13 +77,13 @@ class Sexy_CNN_big(nn.Module):
             Activations of the fully connected layer
         """
         x = self.conv1(x)
-        x = self.pool1(self.alpha1.mul(x).exp()).log().mul(self.alpha1.pow(-1))
+        x = self.pool1((self.upscale1(self.alpha1).mul(x)).exp()).log().mul(self.alpha1.pow(-1))
         x = self.conv2(x)
-        x = self.pool2(self.alpha2.mul(x).exp()).log().mul(self.alpha2.pow(-1))
+        x = self.pool2((self.upscale2(self.alpha2).mul(x)).exp()).log().mul(self.alpha2.pow(-1))
         x = self.conv3(x)
         x = self.conv4(x)
         x = self.conv5(x)
-        x = self.pool3(self.alpha3.mul(x).exp()).log().mul(self.alpha3.pow(-1))
+        x = self.pool3((self.upscale2(self.alpha2).mul(x)).exp()).log().mul(self.alpha2.pow(-1))
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
--- Project/model/__init__.py
+++ Project/model/__init__.py
@@ -4,3 +4,4 @@ from .Sexy_CNN_basic import Sexy_CNN_Basic
 from .Pooled_CNN_big import Pooled_CNN_big
 from .Sexy_CNN_big import Sexy_CNN_big
 from .Spatial_Sexy_CNN_basic import Spatial_Sexy_CNN_Basic
+from .Spatial_Sexy_CNN_big import Spatial_Sexy_CNN_big
