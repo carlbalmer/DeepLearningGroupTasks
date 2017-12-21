ae99b91d27e8eeda715ed16bba954410b2ac5d8b
Carl Balmer <carl.balmer@gmail.com>
2017-12-21 13:48:33 +0100

    added Alpha vizualisation

--- Project/util/Visualize_alpha_matrix.py
+++ Project/util/Visualize_alpha_matrix.py
@@ -1,4 +1,28 @@
-from model import Spatial_Sexy_CNN_Basic
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
-model = Spatial_Sexy_CNN_Basic(10)
-model.load_state_dict()
+alphas = pickle.load(open("data/alphas.pkl", 'rb'))
+
+plt.figure(0)
+plt.imshow(alphas[0], vmin=0, vmax=2.6)
+plt.colorbar()
+plt.title("SpatialSExpLog - Layer 1 - 3-layer-CNN - CIFAR10")
+
+plt.figure(1)
+plt.imshow(alphas[1], vmin=0, vmax=2.6)
+plt.colorbar()
+plt.title("SpatialSExpLog - Layer 1 - 5-layer-CNN - DOGS")
+
+plt.figure(2)
+plt.imshow(alphas[2], vmin=0, vmax=2.6)
+plt.colorbar()
+plt.title("SpatialSExpLog - Layer 2 - 5-layer-CNN - DOGS")
+
+plt.figure(3)
+plt.imshow(alphas[3], vmin=0, vmax=2.6)
+plt.colorbar()
+plt.title("SpatialSExpLog - Layer 3 - 5-layer-CNN - DOGS")
+
+
+plt.show()
